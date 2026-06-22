4514fe0e1a052656bca65f4fa2e2b67cb9a2e579
Mohanakrishna-2301 <185032718+Mohanakrishna-2301@users.noreply.github.com>
2026-06-22 11:35:15 +0530

    Initial commit

---
+++ Module_1/Algorithms_Data_Structures/Exercise_1.java
@@ -0,0 +1,10 @@
+package Module_1.Algorithms_Data_Structures;
+
+public class Exercise_1 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+    }
+    
+}
