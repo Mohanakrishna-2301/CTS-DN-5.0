16072b453b71f215b40bf1186e47073da554fb5f
Mohanakrishna-2301 <185032718+Mohanakrishna-2301@users.noreply.github.com>
2026-06-22 11:33:43 +0530

    Initial commit

---
+++ Module_1/Design_Patterns_and_Principles/Exercise_1.java
@@ -0,0 +1,10 @@
+package Module_1.Design_Patterns_and_Principles;
+
+public class Exercise_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+    
+}
