f5ebe813e8085728049b4a502bc9212eecb873cc
Mohanakrishna-2301 <185032718+Mohanakrishna-2301@users.noreply.github.com>
2026-06-29 16:37:12 +0530

    Search Function

---
+++ module2/ecommercesearch/Product.java
@@ -0,0 +1,23 @@
+package module2.ecommercesearch;
+
+class Product 
+{
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int productId, String productName, String category) 
+    {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Product ID : " + productId +
+                "\nProduct Name : " + productName +
+                "\nCategory : " + category;
+    }
+}
