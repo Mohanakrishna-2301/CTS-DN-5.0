aa990f25af197e26f1800c2e8748ff0b34b6383f
Mohanakrishna-2301 <185032718+Mohanakrishna-2301@users.noreply.github.com>
2026-07-01 13:54:08 +0530

    Junit Basic

---
+++ module4/junit_basic/src/test/java/com/exercise1/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.exercise1;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest 
+{
+    @Test
+    public void testAdd() 
+    {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtract() 
+    {
+        Calculator calc = new Calculator();
+        int result = calc.subtract(10, 4);
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calc = new Calculator();
+        int result = calc.multiply(3, 5);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calc = new Calculator();
+        int result = calc.divide(20, 4);
+        assertEquals(5, result);
+    }
+}
